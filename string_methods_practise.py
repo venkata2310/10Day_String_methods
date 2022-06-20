a41bf926d800a6fc1f8ded91615e3009eaa47310
VENKATA RAGURAM KN <107139560+venkata2310@users.noreply.github.com>
2022-06-20 10:44:28 +0530

    Add files via upload

---
+++ string_methods_practise.py
@@ -0,0 +1,36 @@
+# print('hey python'.capitalize())
+# print('heLlO wORld'.title())
+# print('hey python'.istitle())
+# print('logitech'.upper())
+# print('InTel'.lower())
+# print('EYE IS IMPORTANT'.isupper())
+# print('gold has more value'.islower())
+
+# z='Hi Python'
+# # print(z.replace('Hi','Hey'))
+# print('hello'.count('h'))
+# print('hello'.count('l'))
+# print('hello'.count('lo'))
+
+# print(len('python')) #6
+
+w='visualcode'
+print(w[0])
+print(w[1])
+print(w[2])
+print(w[3])
+print(w[4])
+print(w[5])
+print(w[6])
+print(w[7])
+print(w[8])
+print(w[9])
+
+print(w[-1])
+print(w[-2])
+print(w[-3])
+print(w[-4])
+print(w[-5])
+print(w[-6])
+print(w[-7])
+print(w[-8])
